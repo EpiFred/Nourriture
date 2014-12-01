5ac6d478e01d06ebf98909cb64de8d03844716fd
DiscoPYF <pierreyves.fare@epitech.eu>
2014-12-01 08:22:14 -0800

    deploy_dev

--- bin/deploy_dev.sh
+++ bin/deploy_dev.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-ssh ubuntu@localhost <<EOF
-cd nourriture_dev
-git pull
-sudo ./bin/node_install.sh
-exit
-EOF
+# ssh ubuntu@localhost <<EOF
+# cd nourriture_dev
+# git pull origin develop
+# sudo ./bin/node_install.sh
+# exit
+# EOF
 
 ## TODO: Handle server launch
 
