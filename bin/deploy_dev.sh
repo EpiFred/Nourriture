a5e51a5c6c3b322a80fa438f15e702dccf023592
DiscoPYF <pierreyves.fare@epitech.eu>
2014-12-14 08:12:21 -0800

    server deploy locale issue fixed

--- bin/deploy_dev.sh
+++ bin/deploy_dev.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # ssh ubuntu@localhost <<EOF
+# export LC_CTYPE="en_US.UTF-8"
 # cd nourriture_dev
 # git pull origin develop
 # sudo ./bin/node_install.sh
--- bin/deploy_prod.sh
+++ bin/deploy_prod.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 ssh ubuntu@localhost <<EOF
+export LC_CTYPE="en_US.UTF-8"
 cd nourriture_prod
 git pull
 sudo ./bin/node_install.sh
