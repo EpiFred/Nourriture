69933b1ce9f4c712bd01b10f56ada21b58b1c4d1
DiscoPYF <pierreyves.fare@epitech.eu>
2014-12-14 01:44:41 -0800

    mongodb launch

--- bin/deploy_prod.sh
+++ bin/deploy_prod.sh
@@ -4,6 +4,8 @@ ssh ubuntu@localhost <<EOF
 cd nourriture_prod
 git pull
 sudo ./bin/node_install.sh
+sudo ./bin/mongodb_install.sh
+sudo ./bin/mongodb_launch.sh
 exit
 EOF
 
---
+++ bin/mongodb_launch.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+## Mongodb launch script.
+
+CURRENT_BIN_PATH=`pwd`
+DB_PATH=$CURRENT_BIN_PATH/../source/server/db
+
+if ! [ -e $DB_PATH/mongodb.pid ]; then
+    if [ -d $DB_PATH/data ]; then
+	mkdir $DB_DATA/data
+    fi
+    if [ "$1" = "dev" ] || [ "$1" = "development" ]; then
+	CONFIG=mongodb_dev.conf
+    else
+	CONFIG=mongodb_prod.conf
+    fi
+    mongod --config $DB_PATH/$CONFIG --logpath $DB_PATH/mongodb.log --dbpath $DB_PATH/data --pidfilepath $DB_PATH/mongodb.pid
+fi
