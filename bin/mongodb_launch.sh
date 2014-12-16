cf62f8de5e6668540a1aa364b0cf672ac09ad2fd
DiscoPYF <pierreyves.fare@epitech.eu>
2014-12-16 02:57:02 -0800

    mongodb launch

--- bin/mongodb_launch.sh
+++ bin/mongodb_launch.sh
@@ -6,13 +6,15 @@ CURRENT_BIN_PATH=`pwd`
 DB_PATH=$CURRENT_BIN_PATH/../source/server/db
 
 if ! [ -e $DB_PATH/mongodb.pid ]; then
-    if [ -d $DB_PATH/data ]; then
-	mkdir $DB_DATA/data
+    if ! [ -d $DB_PATH/data ]; then
+	mkdir $DB_PATH/data
     fi
     if [ "$1" = "dev" ] || [ "$1" = "development" ]; then
 	CONFIG=mongodb_dev.conf
     else
 	CONFIG=mongodb_prod.conf
     fi
-    mongod --config $DB_PATH/$CONFIG --logpath $DB_PATH/mongodb.log --dbpath $DB_PATH/data --pidfilepath $DB_PATH/mongodb.pid
+    if ! mongod --config $DB_PATH/$CONFIG --logpath $DB_PATH/mongodb.log --dbpath $DB_PATH/data --pidfilepath $DB_PATH/mongodb.pid; then
+	rm -f $DB_PATH/mongodb.pid
+    fi
 fi
