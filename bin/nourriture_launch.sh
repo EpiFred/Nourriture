787e516bfed43a0b9862fe8e2a78ddad912a3613
DiscoPYF <pierreyves.fare@epitech.eu>
2014-12-17 03:53:26 -0800

    launch

--- bin/nourriture_launch.sh
+++ bin/nourriture_launch.sh
@@ -7,16 +7,18 @@ cd ../source/server
 if [ "$1" = "dev" ] || [ "$1" = "development" ]; then
     UID=nourriture_dev
     npm install
+    BINARY=dev
 else
     UID=nourriture_prod
     npm install --production
+    BINARY=prod
 fi
 
 PIDFILE=$UID.pid
 
 if ! [ -e "$HOME/.forever/pids/$PIDFILE" ]; then
     npm install forever
-    ./node_modules/forever/bin/forever start -p ./ -l forever.log -o output.log -e error.log --uid "$UID" bin/dev
+    ./node_modules/forever/bin/forever start -p ./ -l forever.log -o output.log -e error.log --uid "$UID" bin/$BINARY
 else
     ./node_modules/forever/bin/forever restart "$UID"
 fi
