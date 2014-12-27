1b79cecba2710b22b4fc2547aa821d86c471857d
Julian <julian.hoquang@epitech.eu>
2014-12-27 16:21:42 +0100

    Ajout de la simple connection
    Ajout du package "formidable"
    Connection simple avec la db Account
    TO DO : Generer les tokens et gerer l'authentification

--- source/server/routes/users.js
+++ source/server/routes/users.js
@@ -1,11 +1,110 @@
 var express = require('express');
 var router = express.Router();
+var formidable = require('formidable');
+// ===============================================================================================================================================
+var protocol_login = /^(a-z|A-Z|0-9)[a-zA-Z0-9]+$/;
+var protocol_mail = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                    // 2nd test plus court....: /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+var protocol_password = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/;
+/*
+ *  Function that check if the login is correct
+ *  Return code:
+ *      0 : Everything is ok
+ *      1001 : no field login defined
+ *      1002 : login is empty
+ *      1003 : not conform to the protocol login
+ */
+function CheckLogin(login)
+{
+    if (login === undefined)
+        return ({code: 1001, info: "No field called 'login' in the data sent."});
+    if (login.count == 0)
+        return ({code: 1002, info: "Field 'login' not set in the form."});
+    if (protocol_login.test(login))
+        return ({code: 1003, info: "Field 'login' not conform to the login protocol"});
+    return ({code: 0, info: "Login is OK"});
+}
+
+/*
+ *  Function that check if the password is correct
+ *  Return code:
+ *      0 : Everything is ok
+ *      1011 : No field password defined
+ *      1012 : Password is empty
+ *      1013 : Password is not conform to the protocol (should contain at least one digit,
+ *                                                      should contain at least one lower case,
+ *                                                      should contain at least one upper case,
+ *                                                      should contain at least 8 from the mentioned characters)
+ *
+ */
+function CheckPassword(passwd)
+{
+    if (passwd === undefined)
+        return ({code: 1011, info: "No field called 'password' in the data sent."});
+    if (passwd.count == 0)
+        return ({code: 1012, info: "Field 'password' not set in the form."});
+    if (!protocol_password.test(passwd))
+        return ({code: 1013, info: "Field 'password' not conform to the password protocol"});
+    return ({code: 0, info: "Password is OK"});
+}
+
+/*
+ *  Function that check if the email address is correct
+ *  Return code:
+ *      0 : Everything is ok
+ *      1021 : No field mail defined
+ *      1022 : Mail is empty
+ *      1023 : Mail is not conform to the protocol
+ */
+function CheckMail(mail)
+{
+    if (mail === undefined)
+        return ({code: 1011, info: "No field called 'email' in the data sent."});
+    if (mail.compare("") || mail.count == 0)
+        return ({code: 1012, info: "Field 'email' not set in the form."});
+    if (!protocol_mail.test(mail))
+        return ({code: 1013, info: "Field 'email' not conform to the mail protocol"});
+    return ({code: 0, info: "Mail is OK"});
+}
+
+// ===============================================================================================================================================
+
+/*
+ *  Route for login
+ *  TO DO Manage Token --> Handle other page the Token
+ */
+router.post('/login', function(req, res)
+{
+    var form = new formidable.IncomingForm();
+    var checkError;
+
+    form.parse(req, function(error, formInfos, files)
+    {
+        var login = formInfos.login;
+        var pw = formInfos.password;
+
+        if ((checkError = CheckLogin(login)).code != 0)
+            return (res.send(checkError.info));
+        if ((checkError = CheckPassword(pw)).code != 0)
+            return (res.send(checkError.info));
+
+        var db = req.db;
+        db.collection("account").findOne({Login: login, Password: pw}, function(error, account_res)
+        {
+            if (error)
+                return (res.send("DB Error"));
+            if (account_res === null)
+                res.send("Bad Login or Bad Password");
+            else
+                res.send("Authentication OK");
+        });
+    });
+});
 
 /* GET users listing. */
 router.get('/', function(req, res) {
     res.render('users', { title: 'Nourriture - Users' });
 });
-
 
 /*
  * GET userlist.
@@ -63,6 +162,7 @@ router.put('/editaccount/:id', function(req, res){
         );
     });
 });
+
 
 // ============================================================================================================================================================
 module.exports = router;
