de632c6140278cbff2a810a1c3c649773d9ef996
DiscoPYF <pierreyves.fare@epitech.eu>
2015-01-10 22:50:32 -0800

    clean script

--- bin/nourriture_clean.sh
+++ bin/nourriture_clean.sh
@@ -4,12 +4,8 @@
 
 if [ "$1" = "dev" ] || [ "$1" = "development" ]; then
     UID=nourriture_dev
-    npm install
-    BINARY=dev
 else
     UID=nourriture_prod
-    npm install --production
-    BINARY=prod
 fi
 
 PIDFILE=$UID.pid
