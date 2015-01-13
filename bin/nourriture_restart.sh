2eb4399f1913e01e1edce6ac8582aea92ca62da5
DiscoPYF <pierreyves.fare@epitech.eu>
2015-01-12 19:02:17 -0800

    restart server script

--- bin/nourriture_restart.sh
+++ bin/nourriture_restart.sh
@@ -12,6 +12,12 @@ fi
 UID=nourriture_$MODE
 PIDFILE=$UID.pid
 
-../source/server/node_modules/forever/bin/forever stop --uid "$UID"
-rm -rf "$HOME/.forever/pids/$PIDFILE"
-./nourriture_launch.sh $MODE
+ssh ubuntu@localhost <<EOF
+ export LC_CTYPE="en_US.UTF-8"
+ cd nourriture_$MODE
+ ./source/server/node_modules/forever/bin/forever stop --uid "$UID"
+ cd bin
+ rm -rf "~/.forever/pids/$PIDFILE"
+ ./nourriture_launch.sh dev
+ exit
+EOF
