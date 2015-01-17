a0d96e0de96a45c78bfa24416c0af34c8d72bb78
PYFPYF <pierreyves.fare@epitech.eu>
2015-01-18 00:15:53 +0800

    web directory cleaned.

--- source/web/js/app.js
+++
@@ -1,8 +0,0 @@
-var app = angular.module('MonApp', ['ngRoute']);
-app.config(function($routeProvider){
-	$routeProvider
-	.when('/', {templateUrl: 'partials/home.html'})
-	.when('/search', {templateUrl: 'partials/search.html', controller: 'PostsCtrl'})
-	.when('/comments/:id', {templateUrl: 'partials/recipe.html', controller: 'CommentsCtrl'})
-	.otherwise({redirectTo : '/'});
-});
--- source/web/js/controller/posts.js
+++
@@ -1,9 +0,0 @@
-app.controller('PostsCtrl', function ($scope, PostFactory){
-	$scope.loading = true;
-	$scope.posts = PostFactory.getPosts().then(function(posts){
-		$scope.loading = false;
-		$scope.posts = posts;
-	}, function(msg){
-		alert(msg);
-	})
-});
